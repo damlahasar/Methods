bdde6911994e9c95292c61e2552373199c47f53a
Damla Hasar <hasardamla@gmail.com>
2020-10-31 16:50:25 +0300

    -Vize Final notları girilen öğrencinin ortalamasını bulan metot(0,40-0,60)

---
+++ Example3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example3
+{
+    class Program
+    {
+        private static double NotHesaplama(int vize, int final)
+        {
+            double ortalama = (vize * 0.4 + final * 0.6);
+            return ortalama;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Vize Notunuzu Giriniz: ");
+            int vize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Final Notunuzu Giriniz: ");
+            int final = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ortalamanız: ");
+            Console.WriteLine(NotHesaplama(vize, final));
+            Console.ReadKey();
+        }
+    }
+}
